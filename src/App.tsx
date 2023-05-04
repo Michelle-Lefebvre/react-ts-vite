d1701fdcb93395859272fc9bb68c75d716521c48
Michelle Lefebvre <mmldesigns007@gmail.com>
2023-05-03 21:14:48 -0400

    controlled component

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,17 @@ import Message from "./Message";
 import ListGroup from "./components/ListGroup";
 import Alert from "./components/Alert";
 import { FaBeer } from "react-icons/fa";
+import { AiFillBug } from "react-icons/ai";
 import Like from "./components/Like";
 
 function App() {
   let items = ["New York", "Toronto", "Seattle", "Vancouver", "Montreal"];
   let clicked = 0;
   const [isApproved, setApproved] = useState(true);
+  const handleSelectItem = (item: string) => {
+    clicked++;
+    console.log(item);
+  };
 
   //object updating
   const [drink, setDrink] = useState({
@@ -21,9 +26,26 @@ function App() {
     setDrink({ ...drink, price: 6 });
   };
 
-  const handleSelectItem = (item: string) => {
-    clicked++;
-    console.log(item);
+  // updating array
+  const [tags, setTags] = useState(["happy", "cheerfull"]);
+
+  const handleArrayClick = () => {
+    // add new item
+    setTags([...tags, "exciting"]);
+    // remove
+    setTags(tags.filter((tag) => tag !== "happy"));
+    // update
+    setTags(tags.map((tag) => (tag === "happy" ? "hapiness" : tag)));
+  };
+
+  // updating Array of Object
+  const [bugs, setBugs] = useState([
+    { id: 1, title: "Bug 1", fixed: false },
+    { id: 2, title: "Bug 2", fixed: false },
+  ]);
+
+  const handleBugClick = () => {
+    setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
   };
 
   return (
@@ -40,6 +62,14 @@ function App() {
       <FaBeer onClick={handleDrinkClick} color="brown" size={80} />
       <span>${drink.price}</span>
       <Like onClick={() => console.log("clicked")} />
+      {bugs.map((bug) => (
+        <ul>
+          <li onClick={handleBugClick} key={bug.id}>
+            {bug.id}
+            <AiFillBug />
+          </li>
+        </ul>
+      ))}
     </div>
   );
 }
